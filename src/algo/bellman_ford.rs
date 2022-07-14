29c1926748eec7f5b66878651fa48be17c369df3
zilcH40 <wlh233@live.com>
2022-07-14 22:20:48 +0800

    Add support for multiple predecessors in bellman_ford
    - also add a helper function to find all shortest simple paths

--- src/algo/bellman_ford.rs
+++ src/algo/bellman_ford.rs
@@ -2,6 +2,9 @@
 
 use crate::prelude::*;
 
+use std::collections::HashSet;
+use std::hash::Hash;
+
 use crate::visit::{IntoEdges, IntoNodeIdentifiers, NodeCount, NodeIndexable, VisitMap, Visitable};
 
 use super::{FloatMeasure, NegativeCycle};
@@ -10,6 +13,12 @@ use super::{FloatMeasure, NegativeCycle};
 pub struct Paths<NodeId, EdgeWeight> {
     pub distances: Vec<EdgeWeight>,
     pub predecessors: Vec<Option<NodeId>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct MultiPaths<NodeId, EdgeWeight> {
+    pub distances: Vec<EdgeWeight>,
+    pub predecessors: Vec<Option<Vec<NodeId>>>,
 }
 
 /// \[Generic\] Compute shortest paths from node `source` to all other.
@@ -241,4 +250,184 @@ where
         }
     }
     (distance, predecessor)
+}
+
+/// Same as bellman_ford, but return all shortest paths
+///
+/// # Example
+/// ```rust
+/// use petgraph::Graph;
+/// use petgraph::algo::bellman_ford_multi_predecessors;
+/// use petgraph::prelude::*;
+///
+/// let mut g = Graph::new();
+/// let a = g.add_node(()); // node with no weight
+/// let b = g.add_node(());
+/// let c = g.add_node(());
+/// g.extend_with_edges(&[
+///     (0, 1, 1.0),
+///     (1, 2, 1.0),
+///     (0, 2, 2.0),
+/// ]);
+///
+///
+/// let path = bellman_ford_multi_predecessors(&g, a);
+/// assert!(path.is_ok());
+/// let path = path.unwrap();
+/// assert_eq!(path.distances, vec![    0.0,     1.0,    2.0 ]);
+/// assert_eq!(path.predecessors, vec![None, Some(vec![a]), Some(vec![a, b])]);
+///
+pub fn bellman_ford_multi_predecessors<G>(
+    g: G,
+    source: G::NodeId,
+) -> Result<MultiPaths<G::NodeId, G::EdgeWeight>, NegativeCycle>
+where
+    G: NodeCount + IntoNodeIdentifiers + IntoEdges + NodeIndexable,
+    G::EdgeWeight: FloatMeasure,
+{
+    let ix = |i| g.to_index(i);
+
+    // Step 1 and Step 2: initialize and relax
+    let (distances, predecessors) = bellman_ford_initialize_relax_multi_predecessors(g, source);
+
+    // Step 3: check for negative weight cycle
+    for i in g.node_identifiers() {
+        for edge in g.edges(i) {
+            let j = edge.target();
+            let w = *edge.weight();
+            if distances[ix(i)] + w < distances[ix(j)] {
+                return Err(NegativeCycle(()));
+            }
+        }
+    }
+
+    Ok(MultiPaths {
+        distances,
+        predecessors,
+    })
+}
+
+// Perform Step 1 and Step 2 of the Bellman-Ford algorithm.
+#[inline(always)]
+fn bellman_ford_initialize_relax_multi_predecessors<G>(
+    g: G,
+    source: G::NodeId,
+) -> (Vec<G::EdgeWeight>, Vec<Option<Vec<G::NodeId>>>)
+where
+    G: NodeCount + IntoNodeIdentifiers + IntoEdges + NodeIndexable,
+    G::EdgeWeight: FloatMeasure,
+{
+    // Step 1: initialize graph
+    let mut predecessor = vec![None; g.node_bound()];
+    let mut distance = vec![<_>::infinite(); g.node_bound()];
+    let ix = |i| g.to_index(i);
+    distance[ix(source)] = <_>::zero();
+
+    // Step 2: relax edges repeatedly
+    for _ in 1..g.node_count() {
+        let mut did_update = false;
+        for i in g.node_identifiers() {
+            for edge in g.edges(i) {
+                let j = edge.target();
+                let w = *edge.weight();
+                if distance[ix(i)] + w < distance[ix(j)] {
+                    distance[ix(j)] = distance[ix(i)] + w;
+                    predecessor[ix(j)] = Some(vec![i]);
+                    did_update = true;
+                } else if distance[ix(i)] + w == distance[ix(j)]
+                    && distance[ix(j)] != <_>::infinite()
+                {
+                    // In this branch we find predecessor with same cost
+                    if let Some(v) = &mut predecessor[ix(j)] {
+                        // TODO: need improvement
+                        if !v.contains(&i) {
+                            v.push(i);
+                        }
+                    }
+                }
+            }
+        }
+        if !did_update {
+            break;
+        }
+    }
+    (distance, predecessor)
+}
+
+/// build all shortest simple paths from the result of bellman_ford_multi_predecessors
+/// # Example
+/// ```rust
+/// use petgraph::Graph;
+/// use petgraph::algo::all_shortest_paths;
+/// use petgraph::prelude::*;
+///
+/// let mut g = Graph::new();
+/// let a = g.add_node(()); // node with no weight
+/// let b = g.add_node(());
+/// let c = g.add_node(());
+/// g.extend_with_edges(&[
+///     (0, 1, 1.0),
+///     (1, 2, 1.0),
+///     (0, 2, 2.0),
+/// ]);
+///
+/// let path = all_shortest_paths(&g, a, c).unwrap();
+/// assert_eq!(path, vec![    
+///     vec![a, c],
+///     vec![a, b, c],
+/// ]);
+///
+pub fn all_shortest_paths<G>(
+    g: G,
+    source: G::NodeId,
+    target: G::NodeId,
+) -> Result<Vec<Vec<G::NodeId>>, NegativeCycle>
+where
+    G: NodeCount + IntoNodeIdentifiers + IntoEdges + NodeIndexable,
+    G::NodeId: Eq + Hash,
+    G::EdgeWeight: FloatMeasure,
+{
+    let ix = |i| g.to_index(i);
+    // 1. compute predecessors
+    let paths = bellman_ford_multi_predecessors(&g, source)?;
+    // 2. build paths from predecessors
+    // same as _build_path_from_predecessors from python library networkx
+    let (_dist, pred) = (paths.distances, paths.predecessors);
+    let mut seen: HashSet<_> = vec![target].into_iter().collect();
+    let mut stack: Vec<_> = vec![(target, 0)];
+    let mut ans = vec![];
+    let mut top: isize = 0;
+    while top >= 0 {
+        let (node, i) = stack[top as usize];
+        if node == source {
+            let path = (&stack[..=top as usize])
+                .iter()
+                .rev()
+                .map(|(p, _)| *p)
+                .collect();
+            ans.push(path);
+        }
+        match &pred[ix(node)] {
+            Some(v) if v.len() > i => {
+                stack[top as usize].1 = i + 1;
+                let next = v[i];
+                if seen.contains(&next) {
+                    continue;
+                } else {
+                    seen.insert(next);
+                }
+                top += 1;
+                if top as usize == stack.len() {
+                    stack.push((next, 0));
+                } else {
+                    stack[top as usize] = (next, 0);
+                }
+            }
+            _ => {
+                seen.remove(&node);
+                top -= 1;
+            }
+        }
+    }
+    Ok(ans)
 }
--- src/algo/mod.rs
+++ src/algo/mod.rs
@@ -34,7 +34,7 @@ use crate::visit::Walker;
 use crate::visit::{Data, IntoNodeReferences, NodeRef};
 
 pub use astar::astar;
-pub use bellman_ford::{bellman_ford, find_negative_cycle};
+pub use bellman_ford::{bellman_ford, find_negative_cycle, bellman_ford_multi_predecessors, all_shortest_paths};
 pub use dijkstra::dijkstra;
 pub use feedback_arc_set::greedy_feedback_arc_set;
 pub use floyd_warshall::floyd_warshall;
